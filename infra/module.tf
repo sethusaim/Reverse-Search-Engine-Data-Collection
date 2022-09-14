d342fb0fd1c802746247458d3d0d72292c81312d
sethusaim <sethusaim@gmail.com>
2022-09-14 10:04:42 +0530

    added ecr repo to module.tf

--- infra/module.tf
+++ infra/module.tf
@@ -10,3 +10,6 @@ module "data_collection_app_runner_service" {
   source = "./search_data_collection_app_runner_service"
 }
 
+module "data_collection_ecr_repo" {
+  source = "./search_data_collection_ecr_repo"
+}
